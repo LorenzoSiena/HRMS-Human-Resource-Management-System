120a306699cdc44d22a8d42d77ca4a6ba230e6b9
LorenzoSiena <sienalorenzo91@gmail.com>
2025-03-19 16:48:44 +0100

    Update del dipendente in models

--- hrms_project/hrms_project/settings.py
+++ hrms_project/hrms_project/settings.py
@@ -137,3 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #inserire per t
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'hrms_app.Dipendente'
