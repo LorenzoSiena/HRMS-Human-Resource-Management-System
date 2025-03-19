3e50c2319c035a502d865e64e91cca6ec8a90d94
Pietro411-web <pietro411.92@gmail.com>
2025-03-19 18:04:28 +0100

    ho aggiunto i path , alcune funsioni views ed ho cambiato la redirect del logout

--- hrms_project/hrms_app/urls.py
+++ hrms_project/hrms_app/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", views.user_login, name="login"),
     path("logout", views.user_logout, name="logout"),
+    path("presenza",views.presenze,name="presenze"),
+    path("dipendenti",views.dipendenti,name="dipendenti"),
+    path("stipendi",views.stipendi,name="stipendi"),
 
     # per resettare la password
     path('reset_password/', views.CustomPasswordResetView.as_view(), name='reset_password'),
     path('hrms_app/reset_password_done', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset_password_confirm/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    
 ]
--- hrms_project/hrms_app/views.py
+++ hrms_project/hrms_app/views.py
@@ -9,6 +9,15 @@ from django.contrib import messages
 
 #Model user 
 from django.contrib.auth.models import User
+from .models import Dipendenti
+from .models import Permessi
+from .models import Ruoli
+from .models import Ferie
+from .models import Presenze
+from .models import Bacheca
+from .models import ReportPresenze
+from .models import BustePaga
+from .models import Notifiche
 
 #Form
 from .forms import RegisterForm
@@ -27,6 +36,106 @@ from django.contrib.auth.forms import PasswordResetForm
 
 def hrms_app(request: HttpRequest):
     return render(request,'hrms_app/home.html')
+
+def dipendenti(request:HttpRequest):
+    return render(request,'hrms_app/dipendenti.html')
+
+
+
+def inserisci_dipendente(request:HttpRequest):
+   if request.method =="POST":
+    dipendente = Dipendenti.objects.create(
+        nome=nome,
+        cognome=cognome,
+        email=email,
+        ruolo=ruolo,
+        data_assunzione=data_assunzione,
+        livello_accesso=livello_accesso
+    )
+    messages.success( request , f"✏️ Dipendente '{nome} {cognome}' aggiunto con successo!")
+    return redirect ('dipendente')
+
+   
+
+
+
+def modifica_dipendente( nome, cognome, email, ruolo, data_assunzione, livello_accesso):
+   try:
+    dipendente = Dipendenti.objects.get(id = id_dipendente)
+    dipendente.nome = nuovo_nome
+    dipendente.cognome = nuovo_cognome
+    dipendente.email = nuovo_email
+    dipendente.ruolo = nuovo_ruolo
+    dipendente.data_assunzione = nuovo_data_assunzione
+    dipendente.livello_accesso = nuovo_livello_accesso
+    dipendente.save()
+    print(f"✏️ Dipendente '{id_dipendente}' modificato con successo!")
+   except Bacheca.DoesNotExist:
+        print(f"⚠️ Dipendente '{id_dipendente}' non trovato.")
+
+
+def elimina_dipendente():
+    pass
+
+def presenze(request:HttpRequest):
+    return render(request,'hrms_app/presenze.html')
+
+
+def stipendi(request:HttpRequest):
+    return render(request,'hrms_app/stipendi.html')
+    
+def richiedi_ferie():
+    pass
+
+def archivia_documenti():
+    pass
+
+def notifiche_mail():
+    pass
+
+def aggiungi_messaggio_bacheca(request:HttpRequest):
+    titolo=request.POST.get('titolo')
+    messaggio=request.POST.get('messaggio')
+    nuovo_messaggio = Bacheca.objects.create(titolo=titolo, messaggio=messaggio)
+    messages.success(request,f"✅ Messaggio '{titolo}' aggiunto con successo!")
+    return render(request ,'hrms_app/home.html')
+
+def leggi_messaggio_bacheca():
+    messaggio = Bacheca.objects.all().order_by('data_pubblicazione')
+    if not messaggio:
+      return render('hrms_app/home.html',f"La tua Bacheca è vuota!")
+    else:
+        for msg in messaggio:
+         return render(f"[{msg.data_pubblicazione}] {msg.titolo}: {msg.messaggio}")
+
+def modifica_messaggio(msg_id, nuovo_titolo, nuovo_messaggio):
+    try:
+        messaggio = Bacheca.objects.get(id=msg_id)
+        messaggio.titolo = nuovo_titolo
+        messaggio.messaggio = nuovo_messaggio
+        messaggio.save()
+        print(f"✏️ Messaggio '{msg_id}' modificato con successo!")
+    except Bacheca.DoesNotExist:
+        print(f"⚠️ Messaggio '{msg_id}' non trovato.")
+
+def cancella_messaggio(msg_id):
+    try:
+        messaggio = Bacheca.objects.get(id=msg_id)
+        messaggio.delete()
+        print(f"🗑️ Messaggio '{msg_id}' eliminato con successo!")
+    except Bacheca.DoesNotExist:
+        print(f"⚠️ Messaggio '{msg_id}' non trovato.")
+
+def crea_busta_paga():
+    pass
+
+def visualizza_busta_paga():
+    pass
+
+def report_mensile():
+    pass
+
+
 
 
 def register(request: HttpRequest):
@@ -60,7 +169,7 @@ def user_login(request: HttpRequest):
 
 def user_logout(request: HttpRequest):
     logout(request)
-    return redirect('home')
+    return redirect('login')
 
 
 
