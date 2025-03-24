4cbdca32a0db8a0919a870096467249d7b5b395c
LorenzoSiena <sienalorenzo91@gmail.com>
2025-03-24 15:03:22 +0100

    Registrazione user funzionante!

--- hrms_project/hrms_app/forms.py
+++ hrms_project/hrms_app/forms.py
@@ -7,7 +7,6 @@ from .models import Dipendenti,Ruoli
 
 
 class RegisterForm(UserCreationForm):
-    
     nome = forms.CharField(max_length=100)
     cognome = forms.CharField(max_length=100)
     indirizzo_email = forms.EmailField(required=True)
@@ -28,13 +27,32 @@ class RegisterForm(UserCreationForm):
         fields = ['nome', 'cognome','data_nascita','codice_fiscale','indirizzo_email','telefono','indirizzo_completo','data_assunzione', 'ruolo','superiore', 'stipendio', 'documento_contratto','password1', 'password2']
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-        user.first_name = self.cleaned_data['nome']
-        user.last_name = self.cleaned_data['cognome']
-        user.email = self.cleaned_data['indirizzo_email']
-        
+        # Creiamo l'istanza del modello senza salvarla nel database
+        dipendente: Dipendenti = super().save(commit=False)
+
+        # Assegnazione dei campi personalizzati
+        dipendente.nome = self.cleaned_data['nome']
+        dipendente.cognome = self.cleaned_data['cognome']
+        dipendente.indirizzo_email = self.cleaned_data['indirizzo_email']
+        dipendente.telefono = self.cleaned_data['telefono']
+        dipendente.indirizzo_completo = self.cleaned_data['indirizzo_completo']
+        dipendente.codice_fiscale = self.cleaned_data['codice_fiscale']
+        dipendente.data_nascita = self.cleaned_data['data_nascita']
+        dipendente.data_assunzione = self.cleaned_data['data_assunzione']
+        dipendente.ruolo = self.cleaned_data.get('ruolo')
+        dipendente.superiore = self.cleaned_data.get('superiore')
+        dipendente.stipendio = self.cleaned_data['stipendio']
+
+
+        # Gestione del file caricato
+        if self.cleaned_data.get('documento_contratto'):
+            dipendente.documento_contratto = self.cleaned_data['documento_contratto']
+
+        # Impostazione della password
+        dipendente.set_password(self.cleaned_data["password1"])
+
+        # Salvataggio nel database se commit=True
         if commit:
-            user.save()
-        return user
+            dipendente.save()
 
-
+        return dipendente
